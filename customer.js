fb614753f78907a9d4426143e7d55126cf3e9c60
ehager77 <eric@isabel.io>
2019-04-18 23:38:53 -0700

    updated admin functionality & customer to selct by department

--- customer.js
+++ customer.js
@@ -1,7 +1,6 @@
 var inquirer = require('inquirer');
 var mysql = require('mysql');
-var chalk = require('chalk'); 
-var clui = require('clui');
+var chalk = require('chalk');
 var clear = require('clear')
 var figlet = require('figlet');
 
@@ -32,120 +31,155 @@ function validateInt(value) {
 // Prompt the user for the item & quantity they want to purchase
 function promptUser() {
 
-    // Prompt the user to select an item
+
+    // Prompt the user to select an department
     inquirer.prompt([
         {
-            type: 'input',
+            type: 'list',
             validate: validateInt,
-            name: 'item_id',
-            message: 'Please enter the item id for the product that you would like to purchase.',
-            filter: Number
-        },
-        {
-            type: 'input',
-            validate: validateInt,
-            name: 'quantity',
-            message: 'How many do you need?',
-            filter: Number
+            name: 'option',
+            message: 'Please select a Department:',
+            choices: ['Grocery', 'Produce', 'Cosmetics', 'Clothing', "Pet", "Children", "Pharmacy", "Sports"],
+            filter: function (val) {
+                if (val === 'Grocery') {
+                    return 'grocery';
+                } else if (val === 'Produce') {
+                    return 'produce';
+                } else if (val === 'Cosmetics') {
+                    return 'cosmetics';
+                } else if (val === 'Clothing') {
+                    return 'clothing';
+                }
+                else if (val === 'Pet') {
+                    return 'pet';
+                }
+                else if (val === 'Children') {
+                    return 'children';
+                }
+                else if (val === 'Pharmacy') {
+                    return 'pharmacy';
+                }
+                else if (val === 'Sports') {
+                    return 'sports';
+                }
+                else {
+                    // This case should be unreachable
+                    console.log('ERROR: Unsupported operation!');
+                    exit(1);
+                }
+            }
         }
     ]).then(function (input) {
 
-        var item = input.item_id;
-        var quantity = input.quantity;
+            // Construct the db query string
+            QueryString = 'select * from products where department_name="' + input.option + '"';
+            console.log(QueryString);
 
-        // Query to confirm that the given item ID exists in the desired quantity
-        var queryString = 'SELECT * FROM products WHERE ?';
+            // Make the db query
+            connection.query(QueryString, function (err, data) {
+                if (err) throw err;
 
-        connection.query(queryString, { item_id: item }, function (err, data) {
-            if (err) throw err;
+                console.log('Items in stock: ');
+                console.log('...................\n');
 
-            // If the user has selected an invalid item ID, data attay will be empty
-            // console.log('data = ' + JSON.stringify(data));
+                var strOut = '';
+                for (var i = 0; i < data.length; i++) {
+                    strOut = '';
+                    strOut += 'Item ID: ' + data[i].item_id + '  //  ';
+                    strOut += 'Product Name: ' + data[i].product_name + '  //  ';
+                    strOut += 'Price: $' + data[i].price + '  \n  ';
 
-            if (data.length === 0) {
-                console.log('ERROR: Invalid Item ID. Please select a valid Item ID.');
-                displayInventory();
+                    console.log(strOut);
+                }
 
-            } else {
-                var productData = data[0];
+                // console.log("---------------------------------------------------------------------\n");
+                // Prompt the user to select an item
+                inquirer.prompt([
+                    {
+                        type: 'input',
+                        validate: validateInt,
+                        name: 'item_id',
+                        message: 'Please enter the item id for the product that you would like to purchase.',
+                        filter: Number
+                    },
+                    {
+                        type: 'input',
+                        validate: validateInt,
+                        name: 'quantity',
+                        message: 'How many do you need?',
+                        filter: Number
+                    }
+                ]).then(function (input) {
 
-                // console.log('productData = ' + JSON.stringify(productData));
-                // console.log('productData.stock_quantity = ' + productData.stock_quantity);
+                    var item = input.item_id;
+                    var quantity = input.quantity;
 
-                // If the quantity requested by the user is in stock
-                if (quantity <= productData.stock_quantity) {
-                    console.log('Congratulations, the product you requested is in stock! Placing order!');
+                    // Query to confirm that the given item ID exists in the desired quantity
+                    var queryString = 'SELECT * FROM products WHERE ?';
 
-                    // Construct the updating query string
-                    var updateQueryString = 'UPDATE products SET stock_quantity = ' + (productData.stock_quantity - quantity) + ' WHERE item_id = ' + item;
-                    // console.log('updateQueryString = ' + updateQueryString);
-
-                    // Update the inventory
-                    connection.query(updateQueryString, function (err, data) {
+                    connection.query(queryString, { item_id: item }, function (err, data) {
                         if (err) throw err;
 
-                        console.log('Your oder has been placed! Your total is $' + productData.price * quantity);
-                        console.log('Thank you for shopping with us!');
-                        console.log("\n---------------------------------------------------------------------\n");
+                        // If the user has selected an invalid item ID, data attay will be empty
+                        // console.log('data = ' + JSON.stringify(data));
 
-                        // End the database connection
-                        connection.end();
+                        if (data.length === 0) {
+                            console.log('ERROR: Invalid Item ID. Please select a valid Item ID.');
+                            displayInventory();
+
+                        } else {
+                            var productData = data[0];
+
+                            // console.log('productData = ' + JSON.stringify(productData));
+                            // console.log('productData.stock_quantity = ' + productData.stock_quantity);
+
+                            // If the quantity requested by the user is in stock
+                            if (quantity <= productData.stock_quantity) {
+                                console.log('Congratulations, the product you requested is in stock! Placing order!');
+
+                                // Construct the updating query string
+                                var updateQueryString = 'UPDATE products SET stock_quantity = ' + (productData.stock_quantity - quantity) + ' WHERE item_id = ' + item;
+                                // console.log('updateQueryString = ' + updateQueryString);
+
+                                // Update the inventory
+                                connection.query(updateQueryString, function (err, data) {
+                                    if (err) throw err;
+
+                                    console.log('Your oder has been placed! Your total is $' + productData.price * quantity);
+                                    console.log('Thank you for shopping with us!');
+                                    console.log("\n---------------------------------------------------------------------\n");
+
+                                    // End the database connection
+                                    connection.end();
+                                })
+                            } else {
+                                console.log('Sorry, there is not enough product in stock, your order can not be placed as is.');
+                                console.log('Please modify your order.');
+                                console.log("\n---------------------------------------------------------------------\n");
+
+                                promptUser();
+                            }
+                        }
                     })
-                } else {
-                    console.log('Sorry, there is not enough product in stock, your order can not be placed as is.');
-                    console.log('Please modify your order.');
-                    console.log("\n---------------------------------------------------------------------\n");
-
-                    displayInventory();
-                }
-            }
+                })
+            })
         })
-    })
-}
 
-// displayInventory will retrieve the current inventory from the database and output it to the console
-function displayInventory() {
-    // console.log('___ENTER displayInventory___');
+    }
 
-    // Construct the db query string
-    QueryString = 'SELECT * FROM products';
 
-    // Make the db query
-    connection.query(QueryString, function (err, data) {
-        if (err) throw err;
-
-        console.log('Existing Inventory: ');
-        console.log('...................\n');
-
-        var strOut = '';
-        for (var i = 0; i < data.length; i++) {
-            strOut = '';
-            strOut += 'Item ID: ' + data[i].item_id + '  //  ';
-            strOut += 'Product Name: ' + data[i].product_name + '  //  ';
-            strOut += 'Department: ' + data[i].department_name + '  //  ';
-            strOut += 'Price: $' + data[i].price + '\n';
-
-            console.log(strOut);
-        }
-
-        // console.log("---------------------------------------------------------------------\n");
-
-        //Call 
-        promptUser();
-    })
-}
 
 // Execute the main application logic
-function runBamazon() {
+function bam() {
     clear();
     console.log(
         chalk.yellow(
-            figlet.textSync('Bamazon', { horizontalLayout: 'full' })
+            figlet.textSync('Bam!azon', { horizontalLayout: 'full' })
         )
     );
     // Display the available inventory
-    displayInventory();
+    promptUser();
 }
 
-// Call main
-runBamazon();
+//BAM!!
+bam();
